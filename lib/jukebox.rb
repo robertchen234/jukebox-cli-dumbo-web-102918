2afce50fd8521feb95d0efa706c0229fa83c9d99
Robert Chen <robertchen234@gmail.com>
2018-10-23 00:07:35 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,37 +24,31 @@ def list(songs)
   end
 end
 
- def get_song(input,songs)
-  songs.find do |track|
-    track.start_with?(input) || input.to_i - 1 == songs.index(track)
-  end
-end
-
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
   songs.find do |track|
-    if song_name
-      puts "Playing #{song_name}"
+    if track.include?(input) || input - 1 == songs.index(track)
+      puts "Playing #{track}"
     else
       puts "Invalid input, please try again"
     end
   end
 end
 
- def exit_jukebox
-  puts "Goodbye"
-end
+# def exit_jukebox
+#   puts "Goodbye"
+# end
 
- def run(songs)
-  input = ""
-  help
-  while input != "exit"
-    puts "Please enter a command:"
-    input = gets.chomp.downcase
-    help if input == "help"
-    list(songs) if input == "list"
-    play(songs) if input == "play"
-  end
-  exit_jukebox
- end
+# def run(songs)
+#   input = ""
+#   help
+#   while input != "exit"
+#     puts "Please enter a command:"
+#     input = gets.chomp.downcase
+#     help if input == "help"
+#     list(songs) if input == "list"
+#     play(songs) if input == "play"
+#   end
+#   exit_jukebox
+# end
