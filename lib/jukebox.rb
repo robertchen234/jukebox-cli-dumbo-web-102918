aed6e9855e0aae23b7fab285f81bc5788dcce8f0
Robert Chen <robertchen234@gmail.com>
2018-10-23 00:04:42 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,49 +10,6 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-# def help
-#   #this method should be the same as in jukebox.rb
-#   puts "I accept the following commands:\n- help : displays this help message\n- list : displays a list of songs you can play\n- play : lets you choose a song to play\n- exit : exits this program"
-# end
-
-
-# def list(my_songs)
-#   #this method is different! Collect the keys of the my_songs hash and 
-#   #list the songs by name
-#   i = 0
-#   my_songs.each do |song|
-#     i += 1
-#     puts "#{i}. #{song}" 
-#   end
-# end
-
-
-# def play(my_songs)
-#   #this method is slightly different!
-#   #you should still ask the user for input and collect their song choice
-#   #this time, only allow user's to input a song name
-#   #check to see if the name they give is in fact a key of the my_songs hash
-#   #if it isn't, tell them their choice is invalid
-#   #if it is, play the song using the system 'open <file path>' syntax
-#   #get the file path of the song by looking it up in the my_songs hash
-#   puts "Please enter a song name or number:"
-#   input = gets.chomp
-#   if input.is_a?(Integer)
-#     my_songs.select do |song, location|
-#       location.include?(input.to_s)
-#     end 
-#   end 
-# end
-
-# def exit_jukebox
-#   #this method is the same as in jukebox.rb
-# end
-
-# def run(my_songs)
-#   #this method is the same as in jukebox.rb
-# end
-
-
 def help
   puts "I accept the following commands:
   - help : displays this help message
