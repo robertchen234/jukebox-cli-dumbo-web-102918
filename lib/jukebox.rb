71084f62bc10011ee7abec9f76ae46c7681a8c35
Robert Chen <robertchen234@gmail.com>
2018-10-23 00:06:45 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,29 +17,35 @@ def help
   - play : lets you choose a song to play
   - exit : exits this program"
 end
- def list(songs)
+
+def list(songs)
   songs.each_with_index do |track, i|
     puts "#{i+1}. #{track}"
   end
 end
+
  def get_song(input,songs)
   songs.find do |track|
     track.start_with?(input) || input.to_i - 1 == songs.index(track)
   end
 end
- def play(songs)
+
+def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
-  song_name = get_song(input, songs)
-  if song_name
-    puts "Playing #{song_name}"
-  else
-    puts "Invalid input, please try again"
+  songs.find do |track|
+    if song_name
+      puts "Playing #{song_name}"
+    else
+      puts "Invalid input, please try again"
+    end
   end
 end
+
  def exit_jukebox
   puts "Goodbye"
 end
+
  def run(songs)
   input = ""
   help
